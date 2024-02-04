a3b0786661916fe254b668391b461e547412933b
Mogeko <zhengjunyi@live.com>
2024-02-04 16:11:11 +0100

    build: setup mdx

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({ applyBaseStyles: false })],
+  integrations: [react(), tailwind({ applyBaseStyles: false }), mdx()],
 });
