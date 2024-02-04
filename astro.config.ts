9c97cc5b0ea0b223e2e7770481a1e88c0644cf9f
Mogeko <zhengjunyi@live.com>
2024-02-04 16:17:29 +0100

    build: setup sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -3,8 +3,15 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({ applyBaseStyles: false }), mdx()],
+  site: "https://mogeko.me",
+  integrations: [
+    react(),
+    tailwind({ applyBaseStyles: false }),
+    mdx(),
+    sitemap(),
+  ],
 });
